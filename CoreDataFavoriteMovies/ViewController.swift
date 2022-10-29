f53f13b7b410bdd881ce18ee52306d8085abe474
Parker <parkerrushton@gmail.com>
2022-10-29 00:15:20 -0600

    Initial Commit

---
+++ CoreDataFavoriteMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataFavoriteMovies
+//
+//  Created by Parker Rushton on 10/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
